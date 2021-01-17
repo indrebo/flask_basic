94241e62221f5397f4a641e0fb9a1014edfdb88c
indrebo <t.indrebo@gmail.com>
2021-01-17 19:20:10 +0100

    Chp 57

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 # from flask_migrate import Migrate
+from flask_login import LoginManager, login_manager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
 # migrate = Migrate()
+login_manager = LoginManager()
+login_manager.login_view = 'authentication.do_the_login'
+login_manager.session_protection = 'strong'
+bcrypt = Bcrypt()
 
 
 
@@ -17,6 +23,8 @@ def create_app(config_type):
     db.init_app(app)
     # migrate.init_app(app, db)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
 
     from app.catalog import main
     app.register_blueprint(main)
